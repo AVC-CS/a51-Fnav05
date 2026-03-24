058fb5a00f903396be9fabf00fecd858588f3808
Fnav05 <felvinnavarro10@gmail.com>
2026-03-23 18:52:37 -0700

    A51 Power of 2

--- main.cpp
+++ main.cpp
@@ -7,11 +7,27 @@ using namespace std;
 int main()
 {
     // TODO: declare variable n for the exponent input
+    int n;
+    double result = 1.0;
 
     // TODO: read n from standard input
+    cin >> n;
 
     // TODO: compute 2 to the power of n using a loop (no cmath pow)
-    // For positive n: multiply result by 2, n times
-    // For negative n: divide result by 2, |n| times
+    if (n > 0) {
 
+    // For positive n: multiply result by 2, n times
+    for (int i =0; i < n; i++) {
+        result *= 2;
+    }
 }
+    // For negative n: divide result by 2, |n| times
+    else if (n < 0) {
+        for (int i = 0; i < -n; i++) {
+            result /= 2;
+        }
+    }
+    cout << "2 to Power " << n << " is " << fixed << setprecision(5) << result << endl;
+    return 0;
+}
+
